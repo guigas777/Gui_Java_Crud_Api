5129b78753ca452853007a996df53968ad225071
Guilherme Ferreira <guilherme270406@gmail.com>
2026-04-18 18:00:01 -0300

    feat: adiciona entidade task com JPA

---
+++ src/main/java/tech/gui/api/entity/Task.java
@@ -0,0 +1,40 @@
+package tech.gui.api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private boolean completed = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
